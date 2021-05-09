8e1a5879bd90a4bef44ab4fa8f33494a1d18ac58
Ryan Freeman <N00173767@student.iadt.ie>
2021-05-09 21:33:18 +0100

    Added form validation to Login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -4,10 +4,10 @@ import {useDispatch, useSelector} from 'react-redux';
 
 import actions from '../redux/actions';
 import logo from '../assets/svg/logo.svg';
+import {useFormik} from 'formik';
 
 function Login() {
-    const [email, setEmail] = useState("Raymundo_Bosco89@example.com");
-    const [password, setPassword] = useState("secret");
+    const [loginError, setLoginError] = useState("");
     const {isAuthenticating, isLoggedIn} = useSelector(state => state.auth);
     const dispatch = useDispatch();
     const loginForm = useRef();
@@ -17,13 +17,39 @@ function Login() {
         if (isLoggedIn) history.push('/dashboard');
     }, [isLoggedIn, history]);
 
-    const handleSubmit = function (e) {
-        e.preventDefault();
+    const validate = values => {
+        const errors = {};
 
-        // TODO handle login form validation
+        if (!values.email) {
+            errors.email = 'Email address is required.';
+        } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+            errors.email = 'Email address is invalid.';
+        }
 
-        dispatch(actions.auth.login(email, password));
-    }
+        if (!values.password) {
+            errors.password = 'Password is required.'
+        }
+
+        return errors;
+    };
+
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            password: ''
+        },
+        validate,
+        onSubmit: ({email, password}) => {
+            dispatch(actions.auth.login(email, password))
+                .then(status => {
+                    setLoginError(status);
+                    formik.resetForm();
+                    history.push('/dashboard');
+                }).catch(err => {
+                setLoginError(err);
+            })
+        }
+    })
 
     return (
         <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
@@ -48,30 +74,89 @@ function Login() {
                             <label htmlFor="email" className="block text-sm font-medium text-gray-700">
                                 <span className="sr-only">Email address</span>
                             </label>
-                            <div className="mt-1">
-                                <input id="email" name="email" type="email" autoComplete="email" required
+                            <div className="mt-1 relative">
+                                <input id="email"
+                                       name="email"
+                                       type="email"
+                                       autoComplete="email"
                                        placeholder="Email"
-                                       className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-400 focus:border-blue-400 sm:text-sm"
-                                       value={email}
-                                       onChange={e => setEmail(e.target.value)}
+                                       required
+                                       onChange={formik.handleChange}
+                                       onBlur={formik.handleBlur}
+                                       value={formik.values.email}
+                                       className={`${(formik.touched.email && formik.errors.email)
+                                           ? 'border-red-500'
+                                           : (formik.touched.email && !formik.errors.email)
+                                               ? 'border-green-500'
+                                               : 'border-gray-300'} appearance-none block w-full px-3 py-2 border rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-400 focus:border-blue-400 sm:text-sm`}
                                 />
+                                {(formik.touched.email && formik.errors.email) ? <div
+                                        className="absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
+                                        <svg className="h-5 w-5 text-red-500" xmlns="http://www.w3.org/2000/svg"
+                                             viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                            <path fillRule="evenodd"
+                                                  d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z"
+                                                  clipRule="evenodd"/>
+                                        </svg>
+                                    </div>
+                                    : (formik.touched.email && !formik.errors.email) && <div
+                                    className="absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
+                                    <svg className="h-5 w-5 text-green-500" fill="none"
+                                         stroke="currentColor"
+                                         viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                        <path strokeLinecap="round" strokeLinejoin="round"
+                                              strokeWidth="2"
+                                              d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"/>
+                                    </svg>
+                                </div>
+                                }
                             </div>
+                            {(formik.touched.email && formik.errors.email) &&
+                            <p className="mt-2 text-sm font-medium text-red-500"
+                               id="email-error">{formik.errors.email}</p>}
                         </div>
-
                         <div>
                             <label htmlFor="password" className="block text-sm font-medium text-gray-700">
                                 <span className="sr-only">Password</span>
                             </label>
-                            <div className="mt-1">
+                            <div className="mt-1 relative">
                                 <input id="password" name="password" type="password" autoComplete="current-password"
                                        required
                                        placeholder="Password"
-                                       className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-400 focus:border-blue-400 sm:text-sm"
-                                       value={password}
-                                       onChange={e => setPassword(e.target.value)}/>
+                                       onChange={formik.handleChange}
+                                       onBlur={formik.handleBlur}
+                                       value={formik.values.password}
+                                       className={`${(formik.touched.password && formik.errors.password)
+                                           ? 'border-red-500'
+                                           : (formik.touched.password && !formik.errors.password)
+                                               ? 'border-green-500'
+                                               : 'border-gray-300'} appearance-none block w-full px-3 py-2 border rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-400 focus:border-blue-400 sm:text-sm`}
+                                />
+                                {(formik.touched.password && formik.errors.password) ? <div
+                                        className="absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
+                                        <svg className="h-5 w-5 text-red-500" xmlns="http://www.w3.org/2000/svg"
+                                             viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                            <path fillRule="evenodd"
+                                                  d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z"
+                                                  clipRule="evenodd"/>
+                                        </svg>
+                                    </div>
+                                    : (formik.touched.password && !formik.errors.password) && <div
+                                    className="absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
+                                    <svg className="h-5 w-5 text-green-500" fill="none"
+                                         stroke="currentColor"
+                                         viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                        <path strokeLinecap="round" strokeLinejoin="round"
+                                              strokeWidth="2"
+                                              d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"/>
+                                    </svg>
+                                </div>
+                                }
                             </div>
+                            {(formik.touched.password && formik.errors.password) &&
+                            <p className="mt-2 text-sm font-medium text-red-500"
+                               id="password-error">{formik.errors.password}</p>}
                         </div>
-
                         <div className="flex items-center justify-between">
                             <div className="flex items-center">
                                 <input id="remember_me" name="remember_me" type="checkbox"
@@ -87,10 +172,16 @@ function Login() {
                                 </NavLink>
                             </div>
                         </div>
+                        {loginError &&
+                        <p className="mt-2 text-sm font-medium text-red-500"
+                           id="login-error">{loginError}</p>
+                        }
                         <div>
                             <button type="submit"
                                     className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-500 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-400"
-                                    onClick={handleSubmit}>
+                                    onClick={formik.handleSubmit}
+                                    disabled={isAuthenticating}
+                            >
                                 {isAuthenticating &&
                                 <svg className="animate-spin inline-flex h-5 w-5 text-white"
                                      xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
--- src/redux/actions/auth.js
+++ src/redux/actions/auth.js
@@ -18,29 +18,34 @@ const fetchUser = () => (dispatch) => {
     }
 };
 
-const login = (email, password) => async (dispatch) => {
-    try {
+const login = (email, password) => (dispatch) => {
+    return new Promise((resolve, reject) => {
         dispatch({type: LOGIN_BEGIN});
 
-        const request = await fetch(`${BASE_URL}auth/login`, {
+        fetch(`${BASE_URL}auth/login`, {
             method: 'post',
             headers: {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({email, password})
+        }).then(response => response.json()
+            .then(res => {
+                if (response.status === 200) {
+                    const user = res;
+
+                    localStorage.setItem('user', JSON.stringify(user));
+                    dispatch({type: LOGIN_SUCCESS, payload: user});
+                    resolve("");
+                } else {
+                    dispatch({type: LOGIN_FAILURE});
+                    reject("Email address or password was incorrect.");
+                }
+            })).catch(err => {
+            dispatch({type: LOGIN_FAILURE});
+            reject("Something went wrong.");
         });
-
-        if (request.status === 200) {
-            const user = await request.json();
-
-            localStorage.setItem('user', JSON.stringify(user));
-            dispatch({type: LOGIN_SUCCESS, payload: user});
-        }
-    } catch (err) {
-        console.log(err);
-        dispatch({type: LOGIN_FAILURE});
-    }
+    });
 };
 
 const logout = () => (dispatch) => {
