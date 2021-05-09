edbd5bcaa1878d57e8e5f18c18d21426fe37ae97
Ryan Freeman <N00173767@student.iadt.ie>
2021-05-09 21:36:48 +0100

    Updated API base url

--- src/api/index.js
+++ src/api/index.js
@@ -1,1 +1,3 @@
-export const BASE_URL = 'http://localhost:3000/api/v1/';
+export const BASE_URL = process.env.NODE_ENV === 'development'
+    ? 'http://localhost:3000/api/v1/'
+    : 'https://intellagent-server.herokuapp.com/api/v1/';
