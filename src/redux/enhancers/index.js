ef2dfd23dcd733b436de5b55ba7262909646d753
Ryan Freeman <N00173767@student.iadt.ie>
2021-05-09 02:16:47 +0100

    Remove redux devtools extension in production

--- src/redux/enhancers/index.js
+++ src/redux/enhancers/index.js
@@ -2,8 +2,11 @@ import thunk from 'redux-thunk';
 import {applyMiddleware} from 'redux';
 
 const enhancers = [
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(thunk)
 ]
+
+if (process.env.NODE_ENV === 'development') {
+    enhancers.push(window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+}
 
 export default enhancers;
